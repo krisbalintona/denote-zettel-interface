8fb49c2ce2c3cbf88f309cff1605082ff5a0b88e
Kristoffer Balintona <krisbalintona@gmail.com>
2024-11-12 21:55:34 -0600

    fix: Footnotes in README

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((emacs-lisp-mode . ((sentence-end-double-space . t))))
+((emacs-lisp-mode . ((sentence-end-double-space . t)))
+ (org-mode . ((org-footnote-section . "Footnotes")
+              (org-footnote-define-inline . nil))))
